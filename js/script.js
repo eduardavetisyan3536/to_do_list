3d55d5e814c5433393b4eb28a12480a55e1d0a66
eduardavetisyan3536 <avetisyaneduard61@gmail.com>
2023-05-17 15:09:42 +0400

    to do list

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.querySelector('#push').onclick = function (){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert('Kindly Enter Task Name!!!!') 
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+        <div class = 'task'>
+            <span id = 'taskname'>
+            ${document.querySelector('#newtask input').value}
+            </span>
+            <button class = 'delete'>
+            <i class="far fa-trash-alt"></i>
+            </button>
+            </div>
+        `
+        let current_task = document.querySelectorAll('.delete');
+        for(let i = 0; i < current_task.length; i++){
+            current_task[i].onclick = function (){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
